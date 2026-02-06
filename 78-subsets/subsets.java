dd5ca06bb0ce2415fd898b1c28469c12c6b2adcd
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-02-06 11:53:56 +0530

    Time: 1 ms (87.88%) | Memory: 43.8 MB (96.78%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,34 +1,64 @@
+// class Solution {
+
+//     public List<List<Integer>> subsets(int[] nums) {
+//         return generate(nums, nums.length);
+//     }
+
+//     public List<List<Integer>> generate(int[] nums, int n) {
+
+//         // base case
+//         if (n == 0) {
+//             List<List<Integer>> ans = new ArrayList<>();
+//             ans.add(new ArrayList<>());
+//             return ans;
+//         }
+
+//         // recursion faith
+//         List<List<Integer>> faith = generate(nums, n - 1);
+
+//         // copy old subsets
+//         List<List<Integer>> result = new ArrayList<>(faith);
+
+//         // current element
+//         int num = nums[n - 1];
+
+//         // add current element to each subset
+//         for (List<Integer> list : faith) {
+//             List<Integer> newList = new ArrayList<>(list);
+//             newList.add(num);
+//             result.add(newList);
+//         }
+
+//         return result;
+//     }
+// }
 class Solution {
 
+    List<List<Integer>> answer = new ArrayList<>();
+    int n;
+
     public List<List<Integer>> subsets(int[] nums) {
-        return generate(nums, nums.length);
+        n = nums.length;
+        generate(nums, new ArrayList<>(), 0);
+        return answer;
     }
 
-    public List<List<Integer>> generate(int[] nums, int n) {
+    public void generate(int[] arr, List<Integer> list, int i) {
 
         // base case
-        if (n == 0) {
-            List<List<Integer>> ans = new ArrayList<>();
-            ans.add(new ArrayList<>());
-            return ans;
+        if (i == n) {
+            answer.add(new ArrayList<>(list));
+            return;
         }
 
-        // recursion faith
-        List<List<Integer>> faith = generate(nums, n - 1);
+        // 1️⃣ include current element
+        list.add(arr[i]);
+        generate(arr, list, i + 1);
 
-        // copy old subsets
-        List<List<Integer>> result = new ArrayList<>(faith);
+        // backtrack
+        list.remove(list.size() - 1);
 
-        // current element
-        int num = nums[n - 1];
-
-        // add current element to each subset
-        for (List<Integer> list : faith) {
-            List<Integer> newList = new ArrayList<>(list);
-            newList.add(num);
-            result.add(newList);
-        }
-
-        return result;
+        // 2️⃣ exclude current element
+        generate(arr, list, i + 1);
     }
 }
