e0419f231ad678a59af20389ae5a0271413fac33
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-02-08 11:32:19 +0530

    Time: 4 ms (73.23%) | Memory: 45.3 MB (45.55%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,28 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
-        Stack<Integer> st = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        // Step 1: Find NGE for nums2
-        for (int i = nums2.length - 1; i >= 0; i--) {
-
-            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+        Stack <Integer> st = new Stack<>();
+        HashMap <Integer, Integer> NGElement= new HashMap<>();
+        for(int i =nums2.length-1;i>=0;i--){
+            while(!st.isEmpty()&& st.peek()<= nums2[i]){
                 st.pop();
             }
-
-            int nge = st.isEmpty() ? -1 : st.peek();
-            map.put(nums2[i], nge);
-
+            if(st.isEmpty()){
+                NGElement.put(nums2[i],-1);
+            }
+            else{
+                NGElement.put(nums2[i],st.peek());
+            }
             st.push(nums2[i]);
         }
-
-        // Step 2: Build answer for nums1
-        int[] arr = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            arr[i] = map.get(nums1[i]);
+        int [] result =new int[nums1.length];
+            for(int i =0;i<nums1.length;i++){
+                result[i]=NGElement.get(nums1[i]);
+            }
+            return result;
         }
-
-        return arr;
     }
-}
