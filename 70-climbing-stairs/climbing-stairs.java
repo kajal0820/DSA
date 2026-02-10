e32f7b750f1be13282b3526660f67e111f77ba3e
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-02-10 12:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (51.79%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    int[] dp;
+    public int climbStairs(int n) {
+        dp = new int[n+1];
+        // if(n==0) return 1;
+        // if(n<0) return 0;
+        // int left = climbStairs(n-1);
+        // int right = climbStairs(n-2);
+        // return left +right;
+return climb(n);
+        
+    }
+    public int climb(int n){
+        if(n==0) return 1;
+         if(n<0) return 0;
+         if(dp[n]!=0) return dp[n];
+         int ans= climb(n-1) + climb(n-2);
+         dp[n]= ans;
+         return ans;
+    }
+}
