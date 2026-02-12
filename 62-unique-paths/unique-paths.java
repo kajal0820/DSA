5490870a5713dccc93e48ea0616cbdb80ef94687
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-02-12 10:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (25.97%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,16 +1,25 @@
 class Solution {
-    Integer [][] dp;
-    public int uniquePaths(int m, int n) {
+    Integer[][] dp;
 
-dp =new Integer[m][n];
-return countPaths(0,0,n,m);
-        }
-        public int countPaths(int i, int j, int m, int n){
-            if(i== n-1 && j== m-1 ) return 1;
-            if(i == n || j== m) return 0;
-            if(dp[i][j]!= null) return dp[i][j];
-            int r= countPaths(i,j+1,m,n);
-            int d= countPaths(i+1, j,m,n);
-            return dp[i][j] = r+d;
-        }
+    public int uniquePaths(int m, int n) {
+        dp = new Integer[m][n];
+        return countPaths(0, 0, m, n);
+    }
+
+    public int countPaths(int i, int j, int m, int n) {
+
+        // Reached bottom-right
+        if (i == m - 1 && j == n - 1) return 1;
+
+        // Out of boundary
+        if (i >= m || j >= n) return 0;
+
+        // Already calculated
+        if (dp[i][j] != null) return dp[i][j];
+
+        int right = countPaths(i, j + 1, m, n);
+        int down = countPaths(i + 1, j, m, n);
+
+        return dp[i][j] = right + down;
+    }
 }
