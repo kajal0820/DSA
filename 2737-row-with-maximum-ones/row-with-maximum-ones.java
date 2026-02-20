27e17732d4333dee53efc7607947bd18d2c8adbe
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-02-20 11:09:49 +0530

    Time: 4 ms (85.14%) | Memory: 48.3 MB (75.56%) - LeetSync

---
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        int maxCount = 0;
+        int rowIndex = 0;
+        
+        for(int i = 0; i < n; i++) {
+            int count = 0;
+            
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == 1) {
+                    count++;
+                }
+            }
+            
+            if(count > maxCount) {
+                maxCount = count;
+                rowIndex = i;
+            }
+        }
+        
+        return new int[]{rowIndex, maxCount};
+    }
+}
