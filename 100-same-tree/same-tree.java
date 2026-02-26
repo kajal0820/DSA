ddf2f4ac29140df4acd7d3dd35600133c29c6e37
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-02-26 12:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (94.36%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -19,7 +19,7 @@ class Solution {
       
        if(p== null || q== null) return false;
         if(p.val != q.val) return false;
-       if(p.val != q.val) return false;
+       
        
       return isSameTree(p.left , q.left) && 
       isSameTree(p.right ,q.right);
