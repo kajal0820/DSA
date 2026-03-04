ad1ee26c5dd71f60f0b3a94dda8f4235ab1d54e1
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-03-04 08:29:27 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (82.43%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -3,7 +3,7 @@ class Solution {
         int count=0;
         int n = nums.length;
         for(int i =0;i<nums.length;i++){
-            if(nums[i]>nums[(i+1)%n]){
+            if(nums[i]>nums[(i+1)%n]){  // it also check the last element with the first element . 
             count++;
             }
 
