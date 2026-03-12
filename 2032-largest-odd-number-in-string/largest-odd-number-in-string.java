cfc7714c6c5890d728ae7040374e4501a485966a
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-03-12 07:26:49 +0530

    Time: 1 ms (99.73%) | Memory: 46.9 MB (87.10%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -3,10 +3,10 @@ class Solution {
         for (int i = num.length() - 1; i >= 0; i--) {
             char ch = num.charAt(i);
             int digit = ch - '0';
-            if (digit % 2 == 1) { // rightmost odd digit found
-                return num.substring(0, i + 1); // return prefix up to this digit
+            if (digit % 2 == 1) { 
+                return num.substring(0, i + 1); 
             }
         }
-        return ""; // no odd digit exists
+        return ""; 
     }
 }
