769244a874c1761dcbc426a14d9850adb86bac3c
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-03-18 23:50:27 +0530

    Time: 9 ms (10.48%) | Memory: 168.6 MB (26.61%) - LeetSync

---
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countSubmatrices(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] prefix = new int[m][n];
+        int count = 0;
+
+        // Build prefix sum matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                prefix[i][j] = grid[i][j];
+                if (i > 0) prefix[i][j] += prefix[i-1][j];
+                if (j > 0) prefix[i][j] += prefix[i][j-1];
+                if (i > 0 && j > 0) prefix[i][j] -= prefix[i-1][j-1];
+            }
+        }
+
+        // Count submatrices including (0,0) with sum <= k
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (prefix[i][j] <= k) count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] grid = {{1, 2}, {3, 4}};
+        int k = 4;
+        System.out.println(sol.countSubmatrices(grid, k)); // Output: 3
+    }
+}
