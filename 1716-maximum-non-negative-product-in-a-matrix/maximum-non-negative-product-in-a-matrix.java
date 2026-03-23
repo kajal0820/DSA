7f1b16a2684b25624d977951a1766e4b5d261f37
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-03-23 21:09:34 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (43.56%) - LeetSync

---
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int maxProductPath(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+        long MOD = 1_000_000_007;
+
+        long[][] maxDp = new long[n][m];
+        long[][] minDp = new long[n][m];
+
+        // starting cell
+        maxDp[0][0] = grid[0][0];
+        minDp[0][0] = grid[0][0];
+
+        // first column
+        for(int i = 1; i < n; i++){
+            maxDp[i][0] = maxDp[i-1][0] * grid[i][0];
+            minDp[i][0] = maxDp[i][0];
+        }
+
+        // first row
+        for(int j = 1; j < m; j++){
+            maxDp[0][j] = maxDp[0][j-1] * grid[0][j];
+            minDp[0][j] = maxDp[0][j];
+        }
+
+        // fill DP
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+
+                long val = grid[i][j];
+
+                long a = maxDp[i-1][j] * val;
+                long b = minDp[i-1][j] * val;
+                long c = maxDp[i][j-1] * val;
+                long d = minDp[i][j-1] * val;
+
+                maxDp[i][j] = Math.max(Math.max(a, b), Math.max(c, d));
+                minDp[i][j] = Math.min(Math.min(a, b), Math.min(c, d));
+            }
+        }
+
+        long ans = maxDp[n-1][m-1];
+
+        if(ans < 0) return -1;
+
+        return (int)(ans % MOD);
+    }
+}
