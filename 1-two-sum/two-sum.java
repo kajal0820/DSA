189f17bfda75d186d35183d72bc58fe60c4b9f99
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-03-27 11:35:29 +0530

    Time: 3 ms (58.62%) | Memory: 47.4 MB (17.42%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,25 +1,33 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-    HashMap<Integer, Integer> hp = new HashMap<>();
-    for(int i =0;i<nums.length;i++){
-        int complement = target-nums[i];
-        if(hp.containsKey(complement)){
-            return new int[]{hp.get(complement),i};
-        }
-        hp.put(nums[i],i);
-        }
-        return new int[]{};
-        
-        }  
-          }
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int ans[] = new int[2];
 //         for(int i =0;i<nums.length;i++){
 //             for(int j =i+1;j<nums.length;j++){
-//                 if(nums[i]+nums[j]==target){
-//                     return new int[]{i,j};
+//                 if(nums[i]+ nums[j]== target){
+//                     ans[0]=i;
+//                     ans[1]=j;
+//                     return ans;
 //                 }
 //             }
 //         }
-//         return new int[]{};
-      
+//         return ans;
 //     }
 // }
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int ans[] = new int[2];
+        int num= 0;
+        for(int i =0;i<nums.length;i++){
+            num = target - nums[i];
+            if(map.containsKey(num)){
+                ans[0]= map.get(num);
+                ans[1]= i;
+                return ans;
+            }
+           map.put(nums[i],i);
+            }
+            
+            return ans;
+        }
+    }
