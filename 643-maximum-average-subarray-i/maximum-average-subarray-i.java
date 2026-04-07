9051c3d373a168c28ab4d319ab8c403c2011591d
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-04-07 16:56:34 +0530

    Time: 6 ms (13.72%) | Memory: 69.5 MB (82.74%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -6,20 +6,10 @@ class Solution {
         double maxAvg = Double.NEGATIVE_INFINITY;
 
         for (int right = 0; right < nums.length; right++) {
-
-            
-            sum += nums[right];
-
-           
+              sum += nums[right];
             if (right - left + 1 == k) {
-
-                
                 double avg = (double) sum / k;
-
-                
                 maxAvg = Math.max(maxAvg, avg);
-
-               
                 sum -= nums[left];
                 left++;
             }
