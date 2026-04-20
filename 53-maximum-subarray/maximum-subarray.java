1690cb4a3668aa125a3dc0d44ca00c093703a105
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-04-21 05:03:28 +0530

    Time: 1 ms (99.98%) | Memory: 77.2 MB (69.21%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,33 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-//         int max = Integer.MIN_VALUE;
+        int maxSum = nums[0];
+        int currentSum = nums[0];
 
-//         for (int i = 0; i < nums.length; i++) {
-//             for (int j = i; j < nums.length; j++) {    //brute force
-//                 int sum = 0;
-//                 for (int k = i; k <= j; k++) {
-//                     sum += nums[k];
-//                 }
-//                 max = Math.max(sum, max);
-//             }
-//         }
-
-//         return max;
-//     }
-// }
-      int n = nums.length;
-        int maxi = Integer.MIN_VALUE;
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {    //kadanes algorithm
-            sum += nums[i];
-            maxi = Math.max(maxi, sum);
-
-            if (sum < 0) {
-                sum = 0;
-            }
+        for(int i = 1; i < nums.length; i++){
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
         }
 
-        return maxi;
+        return maxSum;
     }
 }
