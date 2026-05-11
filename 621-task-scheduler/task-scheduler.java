66f89d5e8b7d5a7f08a281a7714673e78e620e56
KAJAL DUBEY <130426994+kajal0820@users.noreply.github.com>
2026-05-11 13:26:10 +0530

    Time: 29 ms (34.59%) | Memory: 48.4 MB (37.47%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,31 +1,49 @@
+import java.util.*;
+class Task implements Comparable<Task>{
+    int frequency;
+    int executionTime;
+    Task(int f, int t){
+        frequency = f;
+        executionTime = t;
+    }
+    public int compareTo(Task that){
+        return that.frequency - this.frequency;
+    }
+}
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-
-        int[] freq = new int[26];
-
-        // count frequency
-        for(char ch : tasks){
-            freq[ch - 'A']++;
+        //freq
+        HashMap<Character,Integer> freqMap = new HashMap<>();
+        for(char ch: tasks){
+            freqMap.put(ch,freqMap.getOrDefault(ch,0)+1);
         }
-
-        int maxFreq = 0;
-
-        // find maximum frequency
-        for(int f : freq){
-            maxFreq = Math.max(maxFreq, f);
+        //insertion in max heap - PQ
+        PriorityQueue<Task> pq = new PriorityQueue<>();
+        // insert tasks in pq
+        for(Character ch : freqMap.keySet()){
+            int freq = freqMap.get(ch);
+            pq.offer(new Task(freq,0));
         }
-
-        int maxCount = 0;
-
-        // count how many tasks have max frequency
-        for(int f : freq){
-            if(f == maxFreq){
-                maxCount++;
+        Queue<Task> queue = new LinkedList<>();
+        int time=0;
+        while(!queue.isEmpty() || !pq.isEmpty()){
+            time++;
+            //check if there is a task in pq & process it
+            if(!pq.isEmpty()){
+                Task task = pq.poll();
+                task.frequency--;
+                if(task.frequency>0){
+                    // update the execution time 
+                    task.executionTime = time + n;
+                    queue.offer(task);
+                }
             }
+            // shift the active process to the pq
+            if(!queue.isEmpty() && queue.peek().executionTime == time){
+                pq.offer(queue.poll());
+            }
+            
         }
-
-        int slots = (maxFreq - 1) * (n + 1) + maxCount;
-
-        return Math.max(tasks.length, slots);
+        return time;
     }
 }
